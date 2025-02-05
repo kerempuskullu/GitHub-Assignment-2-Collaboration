062cedadb839640fd89c6c28c3a4c6ac3368db5c
KaanBilge <101862652+KaanBilge@users.noreply.github.com>
2025-02-05 09:57:46 +0300

    Update ArrayMethods.java
    Added method names

---
+++ ArrayMethods.java
@@ -0,0 +1,21 @@
+public class ArrayMethods {
+    //Finds teh maximum value throughout the array
+    public static void findMax(){
+
+    }
+
+    //Finds the minimum value throughout the array
+    public static void findMin(){
+
+    }
+
+    //Finds the avarage of teh array and displays how much each element differs from the avarage
+    public static void differenceFromAvarage(){
+
+    }
+
+    //Calcuates the sum of od and even indexed elements
+    public static void sumOfOddandEvenElements(){
+
+    }
+}
